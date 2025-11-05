cf68478d1022d26cbba65ce5661f02a8896e8ce1
Rawan-Abdullah <rawan.abdullah020@gmail.com>
2025-11-05 14:32:58 +0300

    Setup pinia , axios , theme store

---
+++ src/modules/dashboard/components/dashboard-current-location.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div>
+    Current location
+  </div>
+</template>
--- src/modules/dashboard/components/dashboard-header.vue
+++ src/modules/dashboard/components/dashboard-header.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import SearchInput from '@/modules/core/components/search-input.vue';
 import ThemeToggle from '@/modules/core/components/theme-toggle.vue';
+import DashboardCurrentLocation from '@/modules/dashboard/components/dashboard-current-location.vue';
+import DashboardSearchInput from '@/modules/dashboard/components/dashboard-search-input.vue';
 
 </script>
 <template>
   <div>
-    <ThemeToggle/>
-<SearchInput />
+    <ThemeToggle />
+    <DashboardSearchInput />
+    <DashboardCurrentLocation />
   </div>
 </template>
